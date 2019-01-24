c904d7f75c87dfd20a08b2eb747cb9c8cc266801
Adrian <adrian.smith151@gmail.com>
2019-01-24 18:49:01 +0000

    indented <li> items

--- src/app/mockMovies.ts
+++ src/app/mockMovies.ts
@@ -15,7 +15,7 @@ export const MOVIES: Movie[] = [
         genres: ['Horror', 'Crime'],
         directors: ['Steven Spielberg','Numan Ass'],
         producers: ['Steven Spielberg', 'Johny'],
-        actors: ['Someone famous', 'Somebody Else', 'A Dick']
+        actors: ['Someone famous', 'Somebody Else', 'A Dickhead']
     },
     {
         title: 'Crap',
