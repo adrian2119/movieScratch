02651ee542548e55052392b40a725e572619d2be
Adrian <adrian.smith151@gmail.com>
2019-01-27 18:39:44 +0000

    add search boxes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
+import { MessagesComponent } from './messages/messages.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    MovieDetailsComponent,
+    MovieSearchComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
